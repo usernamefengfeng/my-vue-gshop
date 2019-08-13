4e866c090512aa718801aa07c1918055606359dc
fengge <fengge@.qq.com>
2019-08-13 15:38:32 +0800

    路由懒加载

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,22 @@
 /* 
   所有路由数组
 */
-import Msite from 'pages/Msite/Msite.vue'
-import Search from 'pages/Search/Search.vue'
-import Order from 'pages/Order/Order.vue'
-import Profile from 'pages/Profile/Profile.vue'
-import Login from 'pages/Login/Login.vue'
+// import Msite from 'pages/Msite/Msite.vue'
+// import Search from 'pages/Search/Search.vue'
+// import Order from 'pages/Order/Order.vue'
+// import Profile from 'pages/Profile/Profile.vue'
+// import Login from 'pages/Login/Login.vue'
+
+/* 
+  路由懒加载------import函数返回的是成功的promise对象------路由组件对象
+*/
+const Msite = () =>import('pages/Msite/Msite.vue') 
+const Search = () =>import('pages/Search/Search.vue') 
+const Order = () =>import('pages/Order/Order.vue') 
+const Profile = () =>import('pages/Profile/Profile.vue') 
+const Login = () =>import('pages/Login/Login.vue') 
+
+
 import Shop from 'pages/Shop/Shop.vue'
 import Goods from 'pages/Shop/Goods/Goods.vue'
 import Ratings from 'pages/Shop/Ratings/Ratings.vue'
